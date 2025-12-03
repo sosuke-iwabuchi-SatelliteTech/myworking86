a0e94d5aedba148a02fb83760bbb2f48067d5f88
sosuke-iwabuchi-SatelliteTech <sosuke-iwabuchi@satellite-tech.co.jp>
2025-12-03 20:29:36 +0900

    プロキシ設定他

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,9 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
+
+        $middleware->trustProxies(at: '*');
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
